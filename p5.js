3612bf271c3f73a3af412916460a3f9db29d9ef9
MichM-809 <mmmorttey@gmail.com>
2026-01-14 19:32:04 -0600

    button

--- p5.js
+++ p5.js
@@ -1,5 +1,8 @@
-function{ setup() {
-    createCanvas(400, 400);
-    background(220);
-}
+let button;
+
+function setup() {
+  createCanvas(400, 400);
+  button = createButton('Click Me');
+  button.position(150, 180);
+  button.mousePressed(changeBackground);
 }
